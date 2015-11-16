77e08820b4c06c4faf8896006ebf67c2c318bc2d
Mark Ormesher <me@markormesher.co.uk>
2015-11-16 10:01:41 +0000

    Import library module into app module

--- super-utils-app/src/main/java/uk/co/markormesher/superutilsapp/RootActivity.java
+++ super-utils-app/src/main/java/uk/co/markormesher/superutilsapp/RootActivity.java
@@ -3,12 +3,13 @@ package uk.co.markormesher.superutilsapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import uk.co.markormesher.superutils.SuperUtils;
 
 public class RootActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		SuperUtils.checkLibraryWorking();
 	}
 }
